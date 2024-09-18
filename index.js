4448015eb88c276508d900cd14270c39b0bd0199
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-09-18 15:50:04 +0200

    fs promise module

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 
-console.log('first');
+// fs.readFile('./data.json', 'utf-8')
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
 
-const fileContent = fs.readFileSync('./data.json', "utf-8");
-
-console.log(fileContent);
-
-console.log('second');
-
-fs.readFile('./data.json', 'utf-8', (error, data) => {
-    if (error) {
-        console.log(error);
-    } else {
+const readFile = async () => {
+    try {
+        const data = await fs.readFile('./data.json', 'utf-8');
         console.log(data);
+    } catch (err) {
+        console.log(err);
     }
-});
+}
 
-console.log('third');
+readFile();
 
-fs.writeFileSync('./new-file.txt', 'Hello world');
-fs.writeFile('./new-file.txt', ' Hellow again', {flag: 'a'}, error => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('file written');
-    }
-});
+// console.log('first');
+
+// const fileContent = fs.readFileSync('./data.json', "utf-8");
+
+// console.log(fileContent);
+
+// console.log('second');
+
+// fs.readFile('./data.json', 'utf-8', (error, data) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// console.log('third');
+
+// fs.writeFileSync('./new-file.txt', 'Hello world');
+// fs.writeFile('./new-file.txt', ' Hellow again', {flag: 'a'}, error => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('file written');
+//     }
+// });
