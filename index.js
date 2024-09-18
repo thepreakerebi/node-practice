8170f211a4e89ec55031eb399dafdc24f502db65
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-09-18 11:15:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require("node:path");
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+
+
---
+++ main.mjs
@@ -0,0 +1,7 @@
+// import * as math from "./math.mjs";
+import { add, subtract } from "./math.mjs";
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+// console.log(math.add(2, 3));
+// console.log(math.subtract(2, 3));
