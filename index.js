acb8dcbddc989b3796f01aa15f30b8e1ccb85da7
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-09-20 00:33:02 +0200

    http routing

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
 
-    res.writeHead(200, {"Content-Type":"text/html"});
-    fs.createReadStream(__dirname + './index.html').pipe(res);
+    if (req.url === '/') {
+        res.writeHead(200, {"Content-Type":"text/html"});
+        fs.createReadStream(__dirname + './index.html').pipe(res);
+    } else if (req.url === '/about') {
+        res.writeHead(200, {"Content-Type":"text/plain"});
+        res.end('This is about page');
+    } else if (req.url === '/api') {
+        res.writeHead(200, {"Content-Type":"application/json"});
+        res.end(JSON.stringify({
+            "food": "rice",
+            "name": "Jollf"
+        }));
+    } else {
+        res.writeHead(404, {"Content-Type":"text/plain"});
+        res.end('Page not found');
+    }
+
+    // res.writeHead(200, {"Content-Type":"text/html"});
+    // fs.createReadStream(__dirname + './index.html').pipe(res);
     // res.end("<h1>Hello world!</h1>");
 });
 
