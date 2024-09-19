ce35c67476ad4d1146b4f2b6d097b8d669eac0a2
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-09-20 00:02:42 +0200

    html response

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
 
-    const superHero = {
-        firstName: 'Bruce',
-        lastName: 'Wayne'
-    }
-
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(superHero));
+    res.writeHead(200, {"Content-Type":"text/html"});
+    fs.createReadStream(__dirname + './index.html').pipe(res);
+    // res.end("<h1>Hello world!</h1>");
 });
 
 server.listen(3000, () => {
